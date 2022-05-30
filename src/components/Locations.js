79345a2c863e849edb9921c7abd6b45ad047ed34
Vitor Afonso <vittokm@hotmail.com>
2022-05-30 10:02:35 +0100

    updated map

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,30 +1,14 @@
 //jshint esversion:9
 
-import React, { useMemo } from 'react';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-import markerIcon from '../icons/icons8-marker-storm-30.png';
-
 export const Locations = ({ section3 }) => {
-  const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.REACT_APP_MAPS_API });
-  const centerMarker = useMemo(() => ({ lat: 51.512591, lng: -0.124704 }), []);
-
   return (
-    <>
-      {isLoaded ? (
-        <section className='section-3 relative z-50' ref={section3}>
-          <h1 className='absolute z-40 top-[65px] left-[180px] text-4xl'>LOCATION</h1>
-          <div className='address-location absolute z-30 top-20 left-40 h-20 w-[150px] bg-white p-2'>
-            <p className='mt-5 text-left paragraph text-xs'>
-              12 Upper ST. Martin's Lane <br /> WC2H 9FB, London
-            </p>
-          </div>
-          <GoogleMap zoom={13} center={centerMarker} mapContainerClassName='w-full h-[50vh] map' options={{ mapId: 'd5822cb2914a8860' }}>
-            <Marker position={centerMarker} icon={markerIcon} />
-          </GoogleMap>
-        </section>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </>
+    <section className='section-3 relative z-50 bg-no-repeat bg-left-top bg-cover min-w-fit h-[50vh]' style={{ backgroundImage: 'url(../../snazzy-image-2.png)' }} ref={section3}>
+      <h1 className='absolute z-40 top-[65px] left-[180px] text-4xl'>LOCATION</h1>
+      <div className='address-location absolute z-30 top-20 left-40 h-20 w-[150px] bg-white p-2'>
+        <p className='mt-5 text-left paragraph text-xs'>
+          12 Upper ST. Martin's Lane <br /> WC2H 9FB, London
+        </p>
+      </div>
+    </section>
   );
 };
